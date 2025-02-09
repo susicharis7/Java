41ed3e4725a63358b32c5042c0a26750d60b8421
susicharis7 <susicharis99@gmail.com>
2025-02-10 00:01:45 +0100

    More exercises...

--- src/main/java/QuestionBank/Conditional_And_Loops/EleventhTask.java
+++ src/main/java/QuestionBank/Conditional_And_Loops/EleventhTask.java
@@ -1,7 +1,32 @@
 package QuestionBank.Conditional_And_Loops;
 
+import java.util.Scanner;
+
 class EleventhTask {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        while(true) {
+            System.out.println("Enter a number (or type done when you are finished) :");
+            String input = sc.next();
+
+            if(input.equals("done")) {
+                break;
+            }
+
+            try {
+                int number = Integer.parseInt(input);
+                sum += number;
+            } catch(NumberFormatException nfe) {
+                System.out.println("Invalid input ! : " + nfe);
+            }
+
+        }
+
+
+        System.out.println("Total sun is: " + sum);
+        sc.close();
     }
 }
