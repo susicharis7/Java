cb7f8fd97a910339dd70f46d8aa448f64c02b3b5
susicharis7 <susicharis99@gmail.com>
2025-03-02 00:02:52 +0100

    Optionals update..

--- src/main/java/Records_And_Optionals/Optionals.java
+++ src/main/java/Records_And_Optionals/Optionals.java
@@ -3,9 +3,15 @@ package Records_And_Optionals;
 // Optional is simply a container for an object. It either contains an object, or doesnt...
 // We use optionals in order to avoid having the null pointer exception when accessing certain objects.
 
+import java.util.Optional;
+
 record Wolf(String name, int age) {
     public static Wolf getWolfByName(String name) {
         return null;
+    }
+
+    public static Optional<Wolf> getWolfByNameOptional(String name) {
+        return Optional.ofNullable(null);
     }
 
     public static void main(String[] args) {
@@ -16,6 +22,14 @@ record Wolf(String name, int age) {
             System.out.println(wolf.age());
         } else {
             System.out.println("No wolf!");
+        }
+
+
+        Optional<Wolf> wolfOptional = getWolfByNameOptional("Strasni");
+        if(wolfOptional.isPresent()) {
+            System.out.println(wolfOptional.get().age());
+        } else if(wolfOptional.isEmpty()) {
+            System.out.println("No value in it!");
         }
     }
 }
