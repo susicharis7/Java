07f8b7a94a6d4dd0bbee586ba789abb9890776ee
susicharis7 <susicharis99@gmail.com>
2025-04-24 23:07:33 +0200

    Testing simple calculator

--- src/test/java/Unit_Testing/SimpleCalculatorTest.java
+++ src/test/java/Unit_Testing/SimpleCalculatorTest.java
@@ -1,4 +1,17 @@
 package Unit_Testing;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SimpleCalculatorTest {
+
+    @Test
+    void addTwoNumbers() {
+        SimpleCalculator numbers = new SimpleCalculator();
+        assertEquals(4,numbers.add(2,2));
+
+    }
+
+
 }
